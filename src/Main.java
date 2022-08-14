e13184c7aac83a477cb06a1f8388c05c6fb9c4cc
OlyaTelesh <telesh.olya@mail.ru>
2022-08-15 00:07:46 +0300

    Создали в main трех клиентов с заданными парамтрами номера счета и суммой и выели их данные о счета на экран

--- src/Main.java
+++ src/Main.java
@@ -3,13 +3,21 @@ public class Main {
         CreditCard Palm = new CreditCard();
         Palm.amountOnAccount = 100;
         Palm.accountNumber = 1000;
+        Palm.accureAmount(400);
 
         CreditCard Saint = new CreditCard();
         Saint.amountOnAccount = 50;
         Saint.accountNumber = 986;
+        Saint.accureAmount(65);
 
         CreditCard Lyi =new CreditCard();
         Lyi.amountOnAccount =15006;
         Lyi.accountNumber = 436;
+        Lyi.minusSum(4);
+
+        Palm.infoAbout();
+        Saint.infoAbout();
+        Lyi.infoAbout();
     }
+
 }
