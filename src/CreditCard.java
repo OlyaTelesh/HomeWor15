667b657aadc7904f14793e45895a89ded38150de
OlyaTelesh <telesh.olya@mail.ru>
2022-08-14 23:42:52 +0300

    Ддобавили метод accureAmount для вывода суммы пополнения счета

--- src/CreditCard.java
+++ src/CreditCard.java
@@ -3,4 +3,10 @@ import java.util.Random;
 public class CreditCard {
     int accountNumber;
     double amountOnAccount;
+
+    void accureAmount(double sum) {
+        double plusSumOnCreditCard = sum;
+        System.out.println("Received on account = " + sum); // сумма поступленеия на аккаунт
+        amountOnAccount = amountOnAccount + sum;
+    }
 }
